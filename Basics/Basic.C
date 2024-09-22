1eab134b01dd11dbe1e5538f6f76ac847cb34633
Mohak-Royal <mohak.royal2112@gmail.com>
2024-09-22 15:52:53 +0530

    Basic , Escape Sequence

---
+++ Basics/Basic.C
@@ -0,0 +1,57 @@
+/*
+	Name: mohak
+	DOC: 21-09-24
+	Purpose : link
+*/
+
+//standar input output
+#include<stdio.h>
+//console input output
+#include<conio.h>
+
+
+
+void main(){
+
+	clrscr();
+
+	printf("Hello World!!");
+
+	getch();
+}
+
+
+
+
+
+
+/*
+stdio:
+
+	- printf : it is used to print or display content on output screen
+		   (console screen)
+
+		   Syntax:
+
+		   printf("hello");
+	- scanf  : it is used to take input from the user or from the console
+		   screen.
+
+
+conio:
+
+
+	- clrscr : it is used to clear the previous output of the program
+
+		Syntax:
+
+		clrscr();
+
+	- getch  : it is used to hold the console screen or output screen
+
+		Syntax:
+
+		getch();
+
+
+*/
---
+++ Basics/ESCAPE.C
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+	clrscr();
+
+	//printf("Hello There!");
+	//printf("I am Mohak.");
+
+	//printf("I am \"Mohak\"");
+
+	//printf("I am 'mohak'");
+
+
+	// new line character : \n
+
+
+	//printf("Hello,\nMy name is \nmohak");
+
+
+	// tab (space) : \t
+
+	//printf("\tMohak\tPatel");
+
+	printf("Mohak\t");
+	printf("\nPatel");
+
+
+	getch();
+}
+
+
+
+/*
+Escape Sequence: \  (backward slash)
+
+"" :   \"
+
+Escape Sequence Characters: \n , \t
+
+	\n : it is used for newline
+	\t : it is used for tab (space)
+
+*/
+
+
