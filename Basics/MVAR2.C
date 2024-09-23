b7a7eb0bcc1f8e5e9243baa143928d5b40441e07
Mohak-Royal <mohak.royal2112@gmail.com>
2024-09-23 17:59:04 +0530

    variable rules

---
+++ Basics/MVAR2.C
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void main(){
+	//int age = 17 , std = 5;
+	//char c1 = 'm' , c2 = 'o';
+
+	int num_1; // valid
+	int num@1; // not valid
+	int num# // not valid
+	int num; // valid
+	int _n ; // valid
+	int 1num; // not valid
+
+	clrscr();
+
+	//printf("Age: %d , standar: %d" , age,std);
+	//printf("\n%c%c" ,c1 , c2);
+	getch();
+}
+
+
+
+// Rules for Variable names:
+
+//  variable names can contain:  alphabets , numbers , underscore
+
+
+// variables names cannot contain special symbols other than underscore
+
+
+// variable names first can only be alphabet or underscore
+
+
+
+
