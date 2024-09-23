974a748517ebbba2f2ffe0058fe02e8ed1343b52
Mohak-Royal <mohak.royal2112@gmail.com>
2024-09-23 17:38:36 +0530

    Variable and AccessModifiers

---
+++ Basics/Variable and accessModifiers.C
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void main(){
+	char c =  'm';
+	//char c;  // declaration
+	clrscr();
+
+	//c = '$';   // initialization
+
+	printf("%c",c);
+
+
+
+
+
+	getch();
+}
+
+
+
+// printf
+// scanf
+// clrscr
+// getch
+
+
+
+// Variable: it is a named location in the memory
+
+	/*
+		Syntax:
+
+		// declaration
+			datatype  variable_name;
+			Example:  int a;
+
+
+		// initialization
+			variable_name = value;
+
+			Example : a = 5;
+
+
+
+		// declaration and initialization together
+
+
+			datatype var_name = value;
+
+			Example:  float pi = 3.14;
+	*/
+
+
+
+
+
+
+// DataType :
+
+	//   number(without point value) : int   // 4  , 8
+	//   number with point value : float   //  3.14
+	//   Alphabets , number , float , symbols : char
+
+
+// Access Modifiers : it is used to denote the type of data.
+
+	// int : %d
+	// float : %f
+	// char : %c
+
+
+
+
+
+
