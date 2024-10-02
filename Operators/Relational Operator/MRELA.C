99408e43b05874369b1c804daf9378ede4d608a0
Mohak-Royal <mohak.royal2112@gmail.com>
2024-10-02 18:50:47 +0530

    relational operators

---
+++ Operators/Relational Operator/MRELA.C
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void main(){
+	int a , b;
+	clrscr();
+
+	a = 50;
+	b = 150;
+
+	// ==
+
+	//printf("== %d" , a == b);
+	//printf("\n!= %d" , a != b);
+	//printf("> %d" , a > b);
+	//printf("<  %d" , a < b);
+
+
+	//printf("%d" , a >= b);
+		  // 150   50
+
+	 printf("%d" , a <= b);
+	      //       150  50
+
+	getch();
+}
+
+
+/*
+2. Realtional Operators:
+
+true(1)  or false(0)
+
+-  Equals To :  == : it checks if the two operands are euqal.
+-  Not Equals To :  != : it checks if the two operands are not equal.
+-  Greater Than :  >  : it checks if the left operand is greater than
+			right operand
+- Less Than : < : it checks if the left operand is lesser than right operand
+
+- Greater than or equals to :  >= : it checks if the left operand is
+				greater or equals to right operand
+
+- less than or equal to : <= : it checks if the left  operand is less or
+				equals to right operand.
+
+*/
